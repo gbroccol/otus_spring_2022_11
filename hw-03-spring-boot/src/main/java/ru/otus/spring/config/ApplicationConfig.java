d4e7b44adfcf8c64cfd9cbd99ff04117f1482d3b
gbroccol <pchelinanst@gmail.com>
2024-01-03 13:12:04 +0100

    First version. HW 03

--- hw-03-spring-boot/src/main/java/ru/otus/spring/config/ApplicationConfig.java
+++ hw-03-spring-boot/src/main/java/ru/otus/spring/config/ApplicationConfig.java
@@ -1,6 +1,6 @@
 package ru.otus.spring.config;
 
-``import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
