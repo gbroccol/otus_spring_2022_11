343484c73be1788b42e88a4eceea6fc800a42648
gbroccol <pchelinanst@gmail.com>
2024-01-12 15:23:47 +0100

    Delete

--- hw-05-spring-jdbc/src/main/java/ru/otus/spring/exception/QuestionsReadingException.java
+++
@@ -1,7 +0,0 @@
-package ru.otus.spring.exception;
-
-public class QuestionsReadingException extends Exception{
-    public QuestionsReadingException(String errorMessage) {
-        super(errorMessage);
-    }
-}
