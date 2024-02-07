95844f07693021abd30d1142b5494c1c001bcbb0
gbroccol <pchelinanst@gmail.com>
2024-02-07 08:57:19 +0100

    Add data for book

---
+++ hw-05-spring-jdbc/src/main/resources/db.changelog/data/2024-02-07--0002-book.sql
@@ -0,0 +1,7 @@
+--liquibase formatted sql
+--changeset gbroccol:2024-02-07-002-book
+
+INSERT INTO book (title, author_id, genre_id)
+VALUES ('Мастер и Маргарита', 1, 4),
+       ('Судьба человека', 2, 3),
+       ('Идиот', 3, 1);
